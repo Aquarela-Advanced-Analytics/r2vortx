67845daf4ebbc81e389d863bc36adfe14d57beb5
Edgar Valente <edvalente@gmail.com>
2017-12-14 14:29:35 -0200

    Closes #17 by creating functions that integrates R to OpenRefine

--- R/refine.r
+++ R/refine.r
@@ -1,6 +1,6 @@
-#' Upload data to local OpenRefine
+#' Push data from R to OpenRefine
 #'
-#' Uploads a DataFrame to an OpenRefine at the same machine
+#' Pushes data from R to OpenRefine as a CSV
 #'
 #' @param data DataFrame to be uploaded
 #' @param name String. Name of project to be created. Default is NULL (Untitled)
@@ -10,7 +10,7 @@
 #' df <- r2vortx::wine
 #' refine_push(df, 'Nameless Project')
 #' }
-refine_push <- function(data, name = NULL){
+refine_push <- function(data, name=NULL){
 
   # Set temporary data file
   temp <- tempfile(pattern = 'refineproject', tmpdir = tempdir(), fileext = '.csv')
@@ -20,7 +20,7 @@ refine_push <- function(data, name = NULL){
   # Set parameters for request
   url <- 'http://localhost:3333/command/core/create-project-from-upload'
   body <- list('project-file' = proj_csv,
-               'project-name' = project.name)
+               'project-name' = name)
 
   # Make request
   resp <- httr::POST(url = url,
@@ -35,9 +35,9 @@ refine_push <- function(data, name = NULL){
   }
 }
 
-#' Downloads csv file from OpenRefine
+#' Pull data from OpenRefine to R
 #'
-#' Extracts a dataset in OpenRefine to R as CSV and loaded as a DataFrame
+#' Pulls data from OpenRefine to R as a DataFrame
 #'
 #' @param id String. ID of project in OpenRefine. Obtainable by clicking 'About' in OpenRefine.
 #' @param name String. Name of project in OpenRefine.
@@ -48,17 +48,18 @@ refine_push <- function(data, name = NULL){
 #' \dontrun{
 #' df <- refine_pull('1622372310646')
 #' }
-refine_pull <- function(id=NULL, name=NULL, format='csv', encoding='UTF-8'){
+refine_pull <- function(name=NULL, id=NULL, encoding='UTF-8'){
 
   # Set parameters for request
   url <- 'http://localhost:3333/command/core/export-rows'
   engine <- list(facets = "", mode = "row-based")
-  body <- list(project = id,
+  project_id <- get_refine_id(name, id)
+  body <-    c(project = project_id,
                engine = engine,
-               format = format)
+               format = 'csv')
 
   # Make request
-  resp <- htth::POST(url = url,
+  resp <- httr::POST(url = url,
                      body = body,
                      encode = 'form')
   parsed <- httr::content(resp, 'text', 'text/csv', encoding)
@@ -67,15 +68,65 @@ refine_pull <- function(id=NULL, name=NULL, format='csv', encoding='UTF-8'){
     stop(print(parsed), call. = FALSE)
   }
 
-
-
   # Read dataset into R
-  df <- readr::read_csv(parsed, col_names = TRUE, col_types = NULL)
+  df <- readr::read_csv(parsed, col_names=TRUE, col_types=NULL)
   return(df)
 
 }
 
-get_refine_id <- function(project_name, project_id){
+#' Gets list of projects in OpenRefine
+#'
+#' @export
+#' @return DataFrame with projects' ID, Name, Last Modified and Number of Rows
+refine_list <- function(){
+
+  cont <- httr::content(httr::GET('http://localhost:3333/command/core/get-all-project-metadata'))
+
+  projects <- data.frame(do.call(rbind, cont$projects))
+  projects$ID <- row.names(projects)
+  projects <- projects[, c('ID', 'name', 'modified', 'rowCount')]
+  names(projects) <- c('ID', 'Name', 'Modified', 'Rows')
+  projects$ID <- as.character(projects$ID)
+  projects$Name <- as.character(projects$Name)
+  projects$Modified <- as.character(projects$Modified)
+  projects$Rows <- as.integer(projects$Rows)
+  projects <- projects[order(projects$Modified, decreasing=TRUE), ]
+  row.names(projects) <- c(1:length(projects[[1]]))
+
+  return(projects)
+
+}
+
+#' Delete OpenRefine project
+#'
+#' Kills project in OpenRefine
+#'
+#' @param id String. ID of project in OpenRefine. Obtainable by clicking 'About' in OpenRefine.
+#' @param name String. Name of project in OpenRefine.
+#' @export
+refine_kill <- function(name=NULL, id=NULL){
+
+  # Set parameters
+  url <- 'http://localhost:3333/command/core/delete-project'
+  project_id <- get_refine_id(name, id)
+  body <- list(project = project_id)
+
+  # Make request
+  resp <- httr::POST(url = url, body = body, encode = 'form')
+
+  if (httr::status_code(resp) >= 300) {
+    stop('Something went wrong...', call. = FALSE)
+  } else {
+    message('Project deleted!')
+  }
+}
+
+#' Gets ID from project in OpenRefine
+#'
+#' @param project_name String with project Name
+#' @param project_id String with project ID
+#' @return String with project ID
+get_refine_id <- function(project_name=NULL, project_id=NULL ){
 
   # If nothing is given, return error
   if (is.null(project_id) & is.null(project_name)){
@@ -83,31 +134,20 @@ get_refine_id <- function(project_name, project_id){
 
   # If only name is given, get ID from metadata
   } else if (is.null(project_id) & !is.null(project_name)){
-    cont <- httr::content(httr::GET('http://localhost:3333/command/core/get-all-project-metadata'))
-    projects <- cont$projects
-    matches <- list()
-    modifieds <- list()
-
-    for (projid in names(projects)){
-      if (projects[[projid]]$name == project_name){
-        matches[[projid]] <- projects[[projid]]$name
-        modifieds[[projid]] <- projects[[projid]]$modified
-      }
-    }
+    proj_list <- refine_list()
 
     # If there's no matching name, return error
-    if (length(matches) == 0){
+    if (length(which(proj_list$Name == project_name)) == 0){
       stop('There is no matching name.')
 
     # If there's only one match, return ID
-    } else if (length(matches) == 1){
-      return(names(matches)[1])
+    } else if (length(which(proj_list$Name == project_name)) == 1){
+      return(proj_list[proj_list$Name == project_name, 'ID'])
 
     # If there's more than one match, return latest
-    } else if (length(matches) > 1){
-      projects_matrix <- matrix(c(matches, modifieds), ncol=2, byrow=TRUE)
-      projects_matrix <- matrix[order(matrix[,2]), ]
-      return(projects_matrix[1,1])
+    } else if (length(which(proj_list$Name == project_name)) > 1){
+      message('There is more than one matching name. Returning latest.')
+      return(proj_list[proj_list$Name == project_name, 'ID'][[1]])
     }
 
   # Finally, if ID has been given, return ID
@@ -115,10 +155,3 @@ get_refine_id <- function(project_name, project_id){
     return(project_id)
   }
 }
-
-# refine_list <- function(){
-#
-#   resp <- httr::GET('http://localhost:3333/command/core/get-all-project-metadata')
-#   parsed <- httr::content(resp, 'parsed')
-#
-# }
